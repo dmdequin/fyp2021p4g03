02d6a9dc637e4c0cc8701f2f6202ae6ffd4bd067
ccor <ccor@itu.dk>
2021-04-29 10:06:40 +0200

    Tokeniser corrections
    Corrections to token.py so that it handles contractions correctly. Also handles '@' and '#'.  Defining output location needs sorting to work better as will always output to the same file regardless of input.

--- scripts/token.py
+++ scripts/token.py
@@ -2,75 +2,107 @@
 
 import re
 import sys
+import csv
 
-line = sys.stdin.readline()
+def tokenise(line):
+    # Initialise lists
+    tokens = []
+    unmatchable = []
 
-# Initialise lists
-tokens = []
-unmatchable = []
-
-# Compile patterns for speedup
-token_pat = re.compile(r"\w+|#|'")
-skippable_pat = re.compile(r'[.,]+')  # typically spaces
-
+    # Compile patterns for speedup
+    token_pat = re.compile(r'\w+|#|\'|@')
+    skippable_pat = re.compile(r'[.,]+')  # typically spaces
 
 
-# As long as there's any material left...
-while line:
-    # Try finding a skippable token delimiter first.
-    skippable_match = re.search(skippable_pat, line)
-    if skippable_match and skippable_match.start() == 0:
-        # If there is one at the beginning of the line, just skip it.
-        line = line[skippable_match.end():]
-    else:
-        # Else try finding a real token.
-        token_match = re.search(token_pat, line)
-        #print(token_match)
-        if token_match and token_match.start() == 0:
-            #print(line[token_match.start():token_match.end()])
-            if line[token_match.start():token_match.end()] == '#':
-                token_match2 = re.search(token_pat, line[1:])
-                tokens.append(line[:token_match2.end()+1])
-                line = line[token_match2.end()+1:]
 
-            elif line[token_match.start():token_match.end()] == "'":
-                token_match2 = re.search(token_pat, line[1:])
-                tokens.append(line[:token_match2.end()+1])
-                line = line[token_match2.end()+1:]
-            # If there is one at the beginning of the line, tokenise it.
+    # As long as there's any material left...
+    while line:
+        # Try finding a skippable token delimiter first.
+        skippable_match = re.search(skippable_pat, line)
+        if skippable_match and skippable_match.start() == 0:
+            # If there is one at the beginning of the line, just skip it.
+            line = line[skippable_match.end():]
+        else:
+            # Else try finding a real token.
+            token_match = re.search(token_pat, line)
+            #print(token_match)
+            if token_match and token_match.start() == 0:
+                #print(line[token_match.start():token_match.end()])
+                if line[token_match.start():token_match.end()] == '#': #keep hash tags together and seperate
+                    try:
+                        token_match2 = re.search(token_pat, line[1:])
+                        tokens.append(line[:token_match2.end()+1])
+                        line = line[token_match2.end()+1:]
+                    except:
+                        line = line[token_match.end():]
+
+                elif line[token_match.start():token_match.end()] == '@': #keep @ tags together and seperate
+                    try:
+                        token_match2 = re.search(token_pat, line[1:])
+                        tokens.append(line[:token_match2.end()+1])
+                        line = line[token_match2.end()+1:]
+                    except:
+                        line = line[token_match.end():]
+
+                elif line[token_match.start():token_match.end()] == "'": #handle contractions as a single word
+                    try:
+                        token_match2 = re.search(token_pat, line[1:])
+                        if ' ' in line[token_match2.start():token_match2.end()]:
+                            line = line[token_match.end():]
+                        
+                        else: 
+                            tokens.append(line[:token_match2.end()+1])
+                            line = line[token_match2.end()+1:]
+                    except:
+                        line = line[token_match.end():]
+
+                # If there is one at the beginning of the line, tokenise it.
+                else:
+                    tokens.append(line[:token_match.end()])
+                    line = line[token_match.end():]
             else:
-                tokens.append(line[:token_match.end()])
-                line = line[token_match.end():]
-        else:
-            # Else there is unmatchable material here.
-            # It ends where a skippable or token match starts, or at the end of the line.
-            unmatchable_end = len(line)
-            if skippable_match:
-                unmatchable_end = skippable_match.start()
-            if token_match:
-                unmatchable_end = min(unmatchable_end, token_match.start())
-            # Add it to unmatchable and discard from line.
-            unmatchable.append(line[:unmatchable_end])
-            line = line[unmatchable_end:]
+                # Else there is unmatchable material here.
+                # It ends where a skippable or token match starts, or at the end of the line.
+                unmatchable_end = len(line)
+                if skippable_match:
+                    unmatchable_end = skippable_match.start()
+                if token_match:
+                    unmatchable_end = min(unmatchable_end, token_match.start())
+                # Add it to unmatchable and discard from line.
+                unmatchable.append(line[:unmatchable_end])
+                line = line[unmatchable_end:]
 
-final_tokens = []
-while len(tokens) > 0:
-    temp1 = tokens.pop(0)
-    try:
-        temp2 = tokens.pop(0)
-        if temp2[0] == "'":
-            temp1 += temp2
+    final_tokens = []
+
+    while len(tokens) > 0:
+        temp1 = tokens.pop(0)
+        try:
+            temp2 = tokens.pop(0)
+            if temp2[0] == "'":
+                temp1 += temp2
+                final_tokens.insert(0, temp1)
+                #print('a', temp1)
+            else:
+                final_tokens.insert(0, temp1)
+                tokens.insert(0, temp2)
+                #print('b', temp1, temp2)
+        except:
             final_tokens.insert(0, temp1)
-            print('a', temp1)
-        else:
-            final_tokens.insert(0, temp1)
-            final_tokens.insert(0, temp2)
-            print('b', temp1, temp2)
-    except:
-        final_tokens.insert(0, temp1)
-        print('d', temp1)
+            #print('d', temp1)
+        
+    final_tokens = final_tokens[::-1]
+
+    #print(final_tokens)
+    #print(unmatchable)
+    return final_tokens
+
+
     
-final_tokens = final_tokens[::-1]
+f = sys.stdin.readlines()
+final = []
+for line in f:
+    final.append(tokenise(line))
 
-print(final_tokens)
-print(unmatchable)
+with open("../datasets/hate/interim/seperated.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(final)
